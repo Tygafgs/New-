0796dff6d3f7c0f86792fda18b876875bf50b29e
Tygafgs <ajibcom8@gmail.com>
2025-09-30 22:07:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lyanakishti_db_user:kvyc9JxgQZvfV80F@cluster0.5gpfiue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "yato")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://lyanakishti_db_user:kvyc9JxgQZvfV80F@cluster0.5gpfiue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Lucy_files')
 
 # ============================
